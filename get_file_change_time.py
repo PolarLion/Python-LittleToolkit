69d17c228bd1c39a4df7b9e181e68dd8dc7c49c5
RainAndSnow <545135082@qq.com>
2014-12-25 10:16:57 +0800

    Update get_file_change_time.py

--- get_file_change_time.py
+++ get_file_change_time.py
@@ -4,30 +4,39 @@
 
 import os
 import sys
-# import datetime
 import time
+import re
 
 if __name__ == "__main__":
-	path = sys.argv[1]
-	# path = ""
-	dirs = os.listdir (path)
-	count_change_time = {}
-	for adir in dirs:
-		afile =  os.path.join (path, adir)
-		if os.path.isfile (afile) == True:
-			dtime = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(os.path.getmtime (afile)))
-			# dtime = str (datetime.timedelta(os.path.getmtime (afile)))
-			if dtime in count_change_time:
-				count_change_time [dtime] += 1
-			else:
-				count_change_time [dtime] = 1
-
-	# filename = ".txt"
-	outfile = open (sys.argv[2], 'w')
-	# outfile = open (filename, 'w')
-	# print ("hello world")
-	# print len (count_change_time)
-	for key in sorted(count_change_time):
-		print str(key) + "\t" + str(count_change_time[key])
-		outfile.write (str(key) + "\t" + str(count_change_time[key]) + "\r\n")
-	outfile.close ()
+  #use your own path
+  path = "D:\\documents\\C++\\lion's c++\\程序00\\c++\\"
+  dirs = os.listdir (path)
+  count_change_time = {}
+  for adir in dirs:
+    afile =  os.path.join (path, adir)
+    if os.path.isfile (afile) == True:
+      t_date = time.strftime ('%Y-%m-%d', time.gmtime (os.path.getmtime (afile)))
+      t_time = time.strftime ('%H:%M:%S', time.gmtime (os.path.getmtime (afile)))
+      if t_date in count_change_time:
+        count_change_time [t_date].append (t_time)
+      else:
+        count_change_time [t_date] = [t_time]
+  #use your own path
+  filename = "C:\\Users\\xuewen\\Desktop\\res.txt"
+  outfile = open (filename, 'w')
+  outfile.write ("date\tnumber of images\tstart time\tend time\r\n")
+  for key in sorted (count_change_time):
+    count_hour = {}
+    time_list = count_change_time [key]
+    print (str (key) + "\t" + str (len (time_list)) + "\t" + min (time_list) + "\t" + max (time_list))
+    outfile.write (str (key) + "\t" + str (len (time_list)) + "\t" + min (time_list) + "\t" + max (time_list) + "\r\n")
+##    for t_time in time_list:
+##      t_hour = t_time [0:2]
+##      if t_hour in count_hour :
+##        count_hour [t_hour] += 1
+##      else:
+##        count_hour [t_hour] = 1
+##    
+##    print str(key) + "\t" + str (len(alist)) + "\t" + str (alist)
+##    outfile.write (str(key) + "\t" + str (len(alist)) + "\t" + str (alist) + "\r\n")
+  outfile.close ()
