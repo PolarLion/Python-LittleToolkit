284925725a270a27b088c70876fd9628510d7866
RainAndSnow <545135082@qq.com>
2015-01-03 21:50:07 +0800

    Update get_file_change_time.py

--- get_file_change_time.py
+++ get_file_change_time.py
@@ -9,7 +9,7 @@ import re
 
 if __name__ == "__main__":
   #use your own path
-  path = "D:\\documents\\C++\\lion's c++\\程序00\\c++\\"
+  path = "D:\\documents\\C++\\C++language\\"
   dirs = os.listdir (path)
   count_change_time = {}
   for adir in dirs:
@@ -22,14 +22,25 @@ if __name__ == "__main__":
       else:
         count_change_time [t_date] = [t_time]
   #use your own path
-  filename = "C:\\Users\\xuewen\\Desktop\\res.txt"
-  outfile = open (filename, 'w')
-  outfile.write ("date\tnumber of images\tstart time\tend time\r\n")
+  filename1 = "C:\\Users\\xuewen\\Desktop\\res1.txt"
+  outfile = open (filename1, 'w')
+  outfile.write ("date\tnumber of images\tstart time\tend time\tnumber of images took from 10:00-14:00\tstart_time\tend_time\r\n")
   for key in sorted (count_change_time):
-    count_hour = {}
+    count_10_to_14 = 0
     time_list = count_change_time [key]
-    print (str (key) + "\t" + str (len (time_list)) + "\t" + min (time_list) + "\t" + max (time_list))
-    outfile.write (str (key) + "\t" + str (len (time_list)) + "\t" + min (time_list) + "\t" + max (time_list) + "\r\n")
+    am_10_start = ""
+    pm_14_end = ""
+    for time in sorted (time_list) :
+      if time >= "10:00:00" and time <= "14:00:00" :
+        if count_10_to_14 == 0 :
+          am_10_start = time
+        if time > pm_14_end or pm_14_end == "MULL" :
+          pm_14_end = time
+        count_10_to_14 += 1
+    print (str (key) + "\t" + str (len (time_list)) + "\t" + min (time_list) + "\t" + max (time_list) + "\t" + str (count_10_to_14) + 
+      "\t" + str (am_10_start) + "\t" + str (pm_14_end) + "\r\n")
+    outfile.write (str (key) + "\t" + str (len (time_list)) + "\t" + min (time_list) + "\t" + max (time_list) + "\t" + str (count_10_to_14) + 
+      "\t" + str (am_10_start) + "\t" + str (pm_14_end) + "\r\n")
 ##    for t_time in time_list:
 ##      t_hour = t_time [0:2]
 ##      if t_hour in count_hour :
