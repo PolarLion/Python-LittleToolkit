549c7c2c15cf5a2ac32ca50f41350a88cd32e8d1
RainAndSnow <545135082@qq.com>
2015-01-05 21:38:36 +0800

    Update get_file_change_time.py

--- get_file_change_time.py
+++ get_file_change_time.py
@@ -20,20 +20,20 @@ def number_to_str_date (year, number) :
   month = "-"
   day = "-"
   if is_leap_year (year) and number <= 366:
-    month_date = [-1, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
+    month_date = [0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366]
   elif number <= 365 :
-    month_date = [-1, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
+    month_date = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365]
   else :
     print ("worng date number")
     return 
   for i in range (1, 13) :
     # print (i)
-    if number < month_date [i] :
+    if number <= month_date [i] :
       if i < 10 :
         month += "0" + str (i)
       else :
         month += str (i)
-      nday = month_date [i] - number
+      nday =  number - month_date [i-1]
       if nday < 10 :
         day += "0" + str (nday)
       else:
@@ -61,19 +61,19 @@ def count_file_by_change_time (input_path, result_file) :
   for adir in dirs:
     afile =  os.path.join (input_path, adir)
     if os.path.isfile (afile) == True:
-      t_date = time.strftime ('%Y-%m-%d', time.gmtime (os.path.getmtime (afile)))
-      t_time = time.strftime ('%H:%M:%S', time.gmtime (os.path.getmtime (afile)))
+      t_date = time.strftime ('%Y-%m-%d', time.gmtime (os.path.getmtime (afile) + 8 * 3600))
+      t_time = time.strftime ('%H:%M:%S', time.gmtime (os.path.getmtime (afile) + 8 * 3600))
       if t_date in count_date:
         count_date [t_date].append (t_time)
       else:
         count_date [t_date] = [t_time]
   
   for year in range (get_year (min (count_date)), get_year (max (count_date))+1) :
-    for days in range (1, get_total_days_of_year (year)) :
+    for days in range (1, get_total_days_of_year (year) + 1) :
       str_day = number_to_str_date (year, days)
       if str_day not in count_date :
         count_date [str_day] = [] 
-    # print year
+  # print year
   outfile = open (result_file, 'w')
   outfile.write ("date\tnumber of images\tstart time\tend time\tnumber of images took from 10:00-14:00\tstart_time\tend_time\r\n")
   for key in sorted (count_date):
